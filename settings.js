8ba36838b7357ee97223edd0eb34e7637903ebbd
Toman10 <102314716+Toman10@users.noreply.github.com>
2022-07-16 16:00:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,7 +54,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['918414802914] //ur owner number
+global.vcardowner = ['918414802914'] //ur owner number
 global.ownername = "Draken サマー" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
